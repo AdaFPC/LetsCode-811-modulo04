88309c4b017767f1b3cd430d39206906143e6232
Ada Ferreira Porto Cruz <adaferreira@gmail.com>
2021-10-21 19:55:22 -0700

    Estado funcional inicial

---
+++ index.js
@@ -0,0 +1,56 @@
+/*Criar um documento html onde tem um form, e nesse form input que o usuario vai colocar o nome, esse nome vai ser add em um lista que vai aparecer embaixo do input e 
+cada nome vai precisar de um botão que vai remover o nome. Usar o preventDefault() */ 
+const userName = document.querySelector('#name')
+
+const form = document.querySelector('form')
+
+const users = document.querySelector('#users')
+let contador = 0
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const {value} = userName
+
+    if(!value || !value.trim()){
+        return
+    }
+    
+    const item = document.createElement('li')
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    const button = document.createElement('button')
+    const id = `id-${contador}`
+
+    item.setAttribute('id', id)
+    button.dataset.parentId = id
+    button.onclick = remove
+    button.setAttribute('class', 'remove')
+
+    span.innerHTML = value
+    item.appendChild(span)
+    item.appendChild(button) 
+    users.appendChild(item)
+    
+    userName.value = ""
+    
+    contador++
+}
+
+function removeFromList(id){
+    let child = null
+
+    for (let item of users.children){
+        if(item.id === id){
+            child = item
+        }    
+    }
+    if(child != null){
+        users.removeChild(child)
+    }
+}
+
+function remove(event){
+    removeFromList(event.target.dataset.parentId)
+}
+   
